77d18fe3b8832cd65fc6539b894416e671d31a4b
bpfeil <benspfeil@gmail.com>
2016-08-20 15:53:08 -0500

    settings missed

---
+++ HomeAutomation/routes/log.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser'); //parses information from POST
+var methodOverride = require('method-override'); //used to manipulate POST
+//var io = require('../socket.io/garage.io');
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(methodOverride(function(req, res){
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method;
+        delete req.body._method;
+        return method;
+      }
+}));
+
+/* GET home page. */
+router.get('/log', function(req, res, next) {
+  res.render('./log', {
+	  title: "Log"
+	});
+});
+
+
+module.exports = router;
